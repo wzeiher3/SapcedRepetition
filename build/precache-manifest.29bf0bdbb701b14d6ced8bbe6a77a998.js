d44d44e1e3f6c0a4a1f64c2046be4b0c863c1942
Will Zeiher <56980103+wzeiher3@users.noreply.github.com>
2020-11-29 21:38:49 -0500

    added deployment

---
+++ build/precache-manifest.29bf0bdbb701b14d6ced8bbe6a77a998.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c80671d0d91cfb1c5d0",
+    "url": "/static/css/main.27595585.chunk.css"
+  },
+  {
+    "revision": "0c80671d0d91cfb1c5d0",
+    "url": "/static/js/main.0c80671d.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "ed03f21d1e72ab906f22",
+    "url": "/static/js/2.ed03f21d.chunk.js"
+  },
+  {
+    "revision": "781d28343052dadd837634f6dff290a8",
+    "url": "/index.html"
+  }
+];
---
+++ src/components/Results/Results.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Button from '../Button/Button';
+import './Results.css'
+
+class Results extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+
+        let resultRender = <p className="results-p">Correct!</p>;
+        if (!this.props.isCorrect) {
+            resultRender = <p className="results-p">Incorrect :(</p>;
+        }
+
+
+        return (
+            <div className="DisplayFeedback">
+                <p >The correct translation for <span className='word-strong'>{this.props.original}</span> was <span className='word-strong'>{this.props.answer}</span> and you chose <span className='word-strong'>{this.props.guess}</span>!</p>
+                <Button className='nextword-button' onClick={this.props.onNextWordClick}>
+                    Try another word!
+                </Button>
+            </div>
+        )
+    }
+}
+
+export default Results;
