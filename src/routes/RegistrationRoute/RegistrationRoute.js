84e3f5d1a7f9d8392e8fa8cc32ef33b73c99e19d
Will Zeiher <56980103+wzeiher3@users.noreply.github.com>
2020-11-30 18:01:17 -0500

    styling changes

--- src/routes/RegistrationRoute/RegistrationRoute.js
+++ src/routes/RegistrationRoute/RegistrationRoute.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import RegistrationForm from '../../components/RegistrationForm/RegistrationForm'
+import './Registration.css'
 
 class RegistrationRoute extends Component {
   static defaultProps = {
